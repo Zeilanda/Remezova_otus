b9ef189f02c722c753095a0c0e7e63812b837eba
Zeilanda <dar.remez@gmail.com>
2021-10-28 17:51:24 +0300

    homework-03 v1.1
    add get-request
    result "message": "pong"

--- homework_03/web/app.py
+++ homework_03/web/app.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello world!!!"}
+    return {"message": "Hello world!"}
 
 
-@app.post("/ping/")
-def create_item(data: dict = Body(...)):
+@app.get("/ping/")
+def get_ping():
     return {
         "message": "pong",
     }
