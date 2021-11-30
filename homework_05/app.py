421e1d7653707b69a7de0dbf5303a67f9310b4a3
Zeilanda <dar.remez@gmail.com>
2021-11-30 19:11:55 +0300

    homework_05
    add base frontend things

---
+++ homework_05/app.py
@@ -0,0 +1,31 @@
+"""
+Домашнее задание №4
+Первое веб-приложение
+
+создайте базовое приложение на Flask
+создайте index view /
+добавьте страницу /about/, добавьте туда текст
+создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
+в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их
+в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
+в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
+"""
+from flask import Flask, Blueprint, render_template
+
+app = Flask(__name__)
+main = Blueprint("main", __name__)
+app.register_blueprint(main, url_prefix='/')
+
+
+@app.route('/', endpoint='home')
+def get_to_home():
+    return render_template('other/home.html')
+
+
+@app.route('/about/', endpoint='about')
+def get_about_info():
+    return render_template('other/about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
